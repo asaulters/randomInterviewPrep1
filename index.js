8fc291f02ea9829d504ec75b765a2d606ce01eff
Adam Saulters <71015650+asaulters@users.noreply.github.com>
2022-09-30 21:35:53 -0400

    updated with more challenges

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 // Write a template string according to the following example:
 // const a = "John";
 // const b = "Joe";
@@ -5,32 +6,36 @@
 // const template = "your template string" ➞ "Their names were:  John,  Joe  and  Jack."
 //const string1 = `Their names were: ${a}, ${b}, and ${c}.`
 
-// modify the template variable to be a template string
+// modify the template variable to be a template string 
 function format(a, b, c) {
-  // the result string must give: "Their names were: a, b and c."
-  const template = `Their names were: ${a}, ${b} and ${c}.`;
-  return template;
-}
+    // the result string must give: "Their names were: a, b and c."
+        const template = `Their names were: ${a}, ${b} and ${c}.`
+        return template
+    }
 
-// Scientists have discovered that in four decades, the world will EXPLODE! It will also
+
+// Scientists have discovered that in four decades, the world will EXPLODE! It will also 
 // take three decades to make a spaceship to travel to a new planet that can hold the entire world population.
 
 // You must calculate the number of people there will be in three decades from now.
 
 // The variable population is the world population now.
 // Assume that every month, someone gives birth to more people n.
-// Return the number of people there will be when the spaceship is complete.
+// Return the number of people there will be when the spaceship is complete.    
 
 //Q- How many people will there be in three decades
 // 3 decades => 360 months
 // mult months x n => total new
 // add to current var
 
+
 function futurePeople(population, n) {
-  let a = n * 360;
-  let total = population + a;
-  return total;
+    let a = n*360;
+    let total = population + a;
+    return total
+
 }
+
 
 // Create a function that accepts an array and returns the last item in the array.
 //Q - Show the last item in the array
@@ -38,11 +43,11 @@ function futurePeople(population, n) {
 // return index of last item
 
 function getLastItem(arr) {
-  const arrLen = arr.length - 1;
-  return arr[arrLen];
+	const arrLen = arr.length -1;
+    return arr[arrLen]
 }
 
-// Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as
+// Write a function that accepts base (decimal), height (decimal) and shape ("triangle", "parallelogram") as 
 // input and calculates the area of that shape.
 // Area of a triangle is 0.5 * b * h
 // Area of a parallelogram is b * h
@@ -53,20 +58,20 @@ function getLastItem(arr) {
 // based upon shape input plug B and H into area formula
 
 function areaShape(base, height, shape) {
-  if (shape === "triangle") {
-    return 0.5 * base * height;
-  } else if (shape === "parallelogram") {
-    return base * height;
-  } else {
-    return "Error, incorrect shape given";
-  }
+	if(shape === 'triangle'){
+        return (0.5 * base * height)
+    } else if(shape === 'parallelogram'){
+        return ( base * height)
+    } else {
+        return "Error, incorrect shape given"
+    }
 }
 
 // Write a function that returns true if a year is a leap, otherwise return false.
-// A year is a "leap year" if it lasts 366 days, instead of 365 in a typical year. That extra day is added to the end of the shortest month,
+// A year is a "leap year" if it lasts 366 days, instead of 365 in a typical year. That extra day is added to the end of the shortest month, 
 // creating February 29.
 
-// A leap year occurs every four years, and will take place if the year is a multiple of four. The exception to this is a year at the beginning
+// A leap year occurs every four years, and will take place if the year is a multiple of four. The exception to this is a year at the beginning 
 // of a century (for example, 1900 or 2000), where the year must be divisible by 400 to be a leap year.
 
 // Q - Is given year a leap year?
@@ -75,19 +80,22 @@ function areaShape(base, height, shape) {
 // if eith true, return true, else, return false
 
 function leapYear(year) {
-  if (year % 4 === 0 || year % 100 === 0) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
+	if (year%4 ===0 || year%100 ===0){
+        console.log("true")
+    } else{
+        console.log('false')
+    }
+
 }
 leapYear(2300);
 
+
 // Create a function that takes an array of numbers or letters and returns a string.
 
+
 function arrayToString(arr) {
-  let a = arr.join("");
-  return a;
+	let a = arr.join('');
+    return a;
 }
 
 // Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.
@@ -95,36 +103,41 @@ function arrayToString(arr) {
 // if odd=> return even, and if even => return odd
 // misread!!
 
+
 function isEvenOrOdd(num) {
-  if (num % 2 === 0) {
-    return "odd";
-  } else {
-    console.log("even");
-  }
+	if(num%2 === 0){
+        return ('odd')
+    } else{
+        console.log('even')
+    }
 }
 
-isEvenOrOdd(10);
+isEvenOrOdd(10)
 
 // According to the lodash documentation, _.drop creates a slice of an array with n elements dropped from the beginning.
 
 // Your challenge is to write your own version using vanilla JavaScript.
 // G - remove n elements from the front of the array
 function drop(arr, val = 1) {
-  return arr.slice(val);
+    return (arr.slice(val))
 }
 
-drop([1, 2, 3], 0);
+drop([1, 2, 3], 0)
 
 // Create a function that takes an array and a string as arguments and returns the index of the string.
 // G - find the index of the given string in the given array
 
 function findIndex(arr, str) {
-  console.log(arr.indexOf(str));
+	console.log(arr.indexOf(str))
 }
 
-findIndex(["a", "b", "c", "d", "e", "f"], "f");
+findIndex(['a', 'b', 'c', 'd', 'e', 'f'], 'f')
 
-// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function
+
+
+
+
+// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function 
 // to detect what the word is, knowing the following rules:
 
 // The wanted word is in lowercase.
@@ -134,150 +147,267 @@ findIndex(["a", "b", "c", "d", "e", "f"], "f");
 // match based upon lowercase
 
 function detectWord(str) {
-  const lowerRules = /[a-z]/g;
-  const wordArr = str.match(lowerRules);
-  return wordArr.join("");
+	const lowerRules = /[a-z]/g;
+	const wordArr = str.match(lowerRules);
+    return wordArr.join('');
 }
 
-detectWord("UcUNFYGaFYFYGtNUH");
+detectWord("UcUNFYGaFYFYGtNUH")
+
 
 // Create a function that finds the index of a given item.
-// find the index
+// find the index 
 // if index isnt there, return -1
 
 function search(arr, item) {
-  return arr.indexOf(item);
+	return arr.indexOf(item);
 }
 
 // Create a function to concatenate two integer arrays.
 
+
 function concat(arr1, arr2) {
-  return arr1.concat(arr2);
+	return arr1.concat(arr2)
+	}
+
+
+
+
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// G - which numbers add to hit the target number
+// loop through nums using two loops
+
+
+var twoSum = function(nums, target) {
+    for(let i=0; i< nums.length; i++){
+        for(let j = i+1; j<nums.length; j++ ){
+            let sum = nums[i] + nums[j]
+            if(sums = target){
+                return[i,j]
+            }
+        }
+    }
+};
+
+// Given an integer x, return true if x is palindrome integer.
+// An integer is a palindrome when it reads the same backward as forward.
+// G - Find out if palindrome
+// take x, turn into a string split intor arr,reverse arr, join, and compare to x
+
+function isPalindrome1(x){
+    let z = toString(x)
+    console.log(z)
+    let y = z.split('')
+        .reverse().join();
+    console.log(y)    
+    let q = parseFloat(y)     
+    console.log(q)
+    if(q === x) {
+        console.log('true')
+    } else {
+        console.log('false')
+    }
+    
 }
 
-// Create a method in the Person class which returns how another person's age compares.
-// Given the instances p1, p2 and p3, which will be initialised with the attributes name and
-// age, return a sentence in the following format:
+// 
 
-// {other person name} is {older than / younger than / the same age as} me.
+isPalindrome1(121)
 
-// G - order ages based upon given person
+function isPalindrome(x){
+    let text = x.toString();
+    for(let i=0; i< text.length/2; i++){
+        if(text.charAt(i) != text.charAt(text.length-1-i))
+        {
+            console.log('false')
+        } 
+    }
+    console.log('true')
+}
+isPalindrome(1221)
 
-// class Person {
-// 	constructor(name, age) {
-// 		this.name = name;
-// 		this.age = age;
-// 	}
+// Given a roman numeral, convert it to an integer.
+// init keys based upon roman numerals
+// init [] of ints
+// loop through s converting as you go and sending numbers into arr
+// when finished add numbers
 
-// 	compareAge(other) {
-//         p1 = new Person("Samuel", 24)
-//         p2 = new Person("Joel", 36)
-//         p3 = new Person("Lily", 24)
+// function romanToInt(s){
+//     let key = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+//     let ans = [];
 
-// 		// Write code here!
+//     for( let i =0; i<s.length; i++){
+//         if(key[s[i]] < key[s[i +1]]) {
+//             // subtract it
+//             ans.push(key[s[i +1]] - key[s[i]]);
+//             i++;
+//             continue;
+//         }
+//         else{
+//             // add to arr
+//             ans.push(key[s[i]])
 
-// 	}
+//         }
+//     }
+//     return ans.reduce((total, current) => total+current)
+
 // }
 
-//
+// romanToInt('I')
+// romanToInt('III')
+// romanToInt('IV')
+// romanToInt('IX')
+// romanToInt('LVIII')
+// romanToInt('MCMXCIV');
+// romanToInt('MMMCMXCIX')
 
-function word(s) {
-  const words = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-  ];
-  console.log(words.indexOf(s));
+// G- find out the value of the roman numeral
+// init key based un given info along with a arr for the sums
+// use for loop to tell if the first letter is one that would cause it to subtact
+
+function romanToInt(s){
+let key ={ I:1, V:5, X:10, L:50, C:100, D:500, M:1000 }
+let ans =[];
+
+for(let i=0; i<s.length; i++){
+    if(key[s[i]] < key[s[i+1]]){
+        // subtract
+        ans.push(key[s[i+1]] - key[s[i]]);
+        i++;
+        continue;
+    } else{
+        // add
+        ans.push(key[s[i]])
+    }
+    console.log(ans.reduce((total, current) => total + current))
 }
 
-word("nine");
 
-// Create a function that searches for the index of a given item in an array. If the item is present, it
-// should return the index, otherwise, it should return -1.
-// G - what is the index of the given item in an array
-// use indexOf to search arr
-
-function search(arr, item) {
-  console.log(arr.indexOf(item));
 }
 
-search([2, 4, 6, 8, 10], 8);
+romanToInt('I')
+romanToInt('III')
+romanToInt('IV')
+romanToInt('IX')
+romanToInt('LVIII')
+romanToInt('MCMXCIV');
+romanToInt('MMMCMXCIX')
 
-// You need to create two functions to substitute toString() and parseInt(); A function called intToString() that converts integers
-// into strings and a function called stringToInt() that converts strings into integers.
-// G - change string to int or vice versa
-function intToString(num) {
-  return num.toString();
+
+
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+// G - find is there a common prefix, if so what is it, if not return empty string
+//init arr to hold characters,
+// loop through to pull the 0 index of each string
+// check to see if they are the same
+// repeat until not the same
+// return the arr
+
+
+// var longestCommonPrefix = function(strs) {
+//     let chars = [];
+//     for(var i =0; i<strs.length; i++){
+//         var tmpChar = strs[0[i]];
+//         for(var j=o; j<strs.length; j++){
+//             if(strs[k].length )
+//         };
+        
+        
+//         console.log(strs[i][0]);
+//         if(strs[i[0]] ===strs[i+1[0]] ){
+//             console.log('equal')
+//             chars.push(strs[i[0]])
+//             console.log(chars)
+//         }
+        
+//     }
+// };
+
+// longestCommonPrefix(["flower","flow","flight"])
+
+
+// Running sum not a find the sum!!
+// G - find the running sum
+// running sum is the sum of each number of the index with the prev indexes added to it
+
+var runningSum = function(nums){
+    for(let i = 1; i< nums.length; i++){
+            nums[i] += nums[i-1];
+    }
+    return nums
 }
+runningSum([1, 2, 3, 4])
 
-function stringToInt(num) {
-  return parseInt(num);
-}
 
-// Given a Rubik's Cube with a side length of n, return the number of individual stickers that are needed to cover the whole cube.
-// g - How many stickers does a cube with a side length of n have
-// determine the exponent of n
-// multiply by 6 (6 sides to a cube)
 
-function howManyStickers(n) {
-  let sideStickers = n * n;
-  console.log(sideStickers * 6);
-}
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
 
-howManyStickers(14);
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+// G - find who the richest customer is
+// init global var to take highest sum
+// foreach customer => init currWealth, loop though that customer and add sum to currWealth
+// compare to maxWealth
+// if larger replace
 
-//
-// *****************************************************************************************************************************************************// *****************************************************************************************************************************************************
+var maximumWealth = function(accounts) {
+    var maxWealthSoFar = 0;
 
-// *****************************************************************************************************************************************************
+    accounts.forEach(customer => {
+        let currCustWealth = 0;
+        customer.forEach(bank =>{
+            currCustWealth += bank
+            console.log(currCustWealth);
+            if(currCustWealth > maxWealthSoFar){
+                maxWealthSoFar = currCustWealth
+            }
+        })
+        // customer.reduce();
 
-// *****************************************************************************************************************************************************
+        
+    });
+return maxWealthSoFar
 
-// *****************************************************************************************************************************************************
-// *****************************************************************************************************************************************************
-// *****************************************************************************************************************************************************
+};
 
-//
+maximumWealth([[2,8,7],[7,1,3],[1,9,5]])
 
-// Write a function that will return if string is a palindrome
-// G - return if string is a palindrome
-// split str into arr
-// reverse arr
-//
 
-function isPalindrome(str) {
-  return str.split("").reverse().join("") === str;
-}
+// Given an integer n, return a string array answer (1-indexed) where:
 
-console.log(isPalindrome("racecar"));
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+// G - print out the given string, returning fizz if / by 3, buzz if / 5, fixxBuzz if / by both, or the int(as a str) if neither / by 3 or 5
+// loop though n using if statements
 
-// write a function that will return min and max number in arr
-// G - return min and max number in arr
+function fizzBuzz(n){
+    let answer = '';
+    for(let i =1; i<=n; i++){
+        let ans = '';
+        if(i%5 === 0 &&  i % 3 === 0){
+            ans+= 'FizzBuzz, ';
+        }
+        else if ( i % 3 === 0 ){ 
+            ans +='Fizz, ';
+        }
+        else if (i%5 === 0 ){
+            ans += 'Buzz, ';
+        }
+        else { 
+            ans += i +', ';
+        }
+        answer += ans
+    }
+    console.log(answer)    
+};
 
-function minMax(arr) {
-  let min = Math.min(...arr);
-  let max = Math.max(...arr);
-  console.log(min, max);
-}
 
-minMax([1, 5, 6, 54, 12, -5]);
 
-// write a function that will verufyif str has balanced parenthesis
-// Apparently common at higher end companies?
-// G - find out if parenthese is balannced
-// find middle two and see if they are the same,
-// if so remove and check next, if not return not balanced
-//  OR
-// use first and last, if same => shift and pop
-function balancedPar(str1) {
-  let stack = [];
-}
-
-balancedPar("{{()}}");
+fizzBuzz(15)
