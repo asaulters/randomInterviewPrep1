c86ea5dd7007e9dcdf6cff6acd141b979ac26b96
Adam Saulters <71015650+asaulters@users.noreply.github.com>
2022-10-11 21:09:57 -0400

    Added another answer

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 // Write a template string according to the following example:
 // const a = "John";
 // const b = "Joe";
@@ -557,3 +556,21 @@ function duplicateZeros(arr) {
 
 duplicateZeros([1,0,2,3,0,4,5,0])
 
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+// G - remove all occurrences of val in arr
+// loop through arr
+// if arr[i] === val, arr[i].splice
+
+function removeElement(nums, val) {
+    if(!nums) return 0;
+    for(let i =0; i< nums.length; i++){
+        if(nums[i] ===val){
+            nums.splice(i, 1);
+            i--
+        }
+    }
+    return nums.length
+};
+
+removeElement([0,1,2,2,3,0,4,2], 2)
