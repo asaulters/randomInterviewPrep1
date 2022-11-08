6ecb20989f2ab8e0f55e1fdd9ebd1bba686e6e95
Adam Saulters <71015650+asaulters@users.noreply.github.com>
2022-11-07 20:59:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -192,6 +192,9 @@ var twoSum = function(nums, target) {
     }
 };
 
+
+
+
 // Given an integer x, return true if x is palindrome integer.
 // An integer is a palindrome when it reads the same backward as forward.
 // G - Find out if palindrome
